2f5ec5e4a37df6cdce41d4766d627842e91344da
Loulfy <loulfy.dev@gmail.com>
2018-11-29 20:42:28 +0100

    add wordborder protect

---
+++ src/main/java/io/lylix/ericius/Ericius.java
@@ -0,0 +1,134 @@
+package io.lylix.ericius;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.border.WorldBorder;
+import net.minecraftforge.common.DimensionManager;
+import net.minecraftforge.common.util.ITeleporter;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@Mod.EventBusSubscriber
+@Mod(modid = Ericius.MODID, name = Ericius.NAME, version = Ericius.VERSION, serverSideOnly = true, acceptableRemoteVersions = "*")
+public class Ericius
+{
+    public static final String MODID = "ericius";
+    public static final String NAME = "Ericius";
+    public static final String VERSION = "${version}";
+
+    @Mod.Instance
+    public static Ericius instance;
+
+    private static Logger logger;
+
+    private static Map<Integer, BlockPos> spawn = new HashMap<>();
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+        parse(event.getModConfigurationDirectory());
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @SubscribeEvent
+    public static void onChangeDimension(PlayerEvent.PlayerChangedDimensionEvent event)
+    {
+        WorldServer world = DimensionManager.getWorld(event.toDim);
+        WorldBorder border = world.getWorldBorder();
+        if(border.getClosestDistance(event.player) < 0)
+        {
+            Ericius.logger.info("WorldBorderRedirect : {} from {} to {}", event.player.getName(), event.fromDim, event.toDim);
+            MinecraftServer s = FMLCommonHandler.instance().getMinecraftServerInstance();
+            s.getPlayerList().transferPlayerToDimension((EntityPlayerMP) event.player, event.toDim, new TeleportToSpawn());
+        }
+    }
+
+    public void parse(File dir)
+    {
+        File file = new File(dir, "ericius-spawn.txt");
+        Properties props = new Properties();
+
+        try
+        {
+            if(file.createNewFile()) logger.warn("create spawn file");
+            props.load(new FileInputStream(file));
+        }
+        catch (IOException e)
+        {
+            logger.warn("can not parse spawn file");
+        }
+
+        for(String key : props.stringPropertyNames())
+        {
+            try
+            {
+                final String[] coords = props.getProperty(key).split(",");
+                if(coords.length < 3) continue;
+
+
+                int d = Integer.valueOf(key);
+                int x = Integer.valueOf(coords[0]);
+                int y = Integer.valueOf(coords[1]);
+                int z = Integer.valueOf(coords[2]);
+
+                BlockPos p = new BlockPos(x,y,z);
+                logger.info("register spawn : {} to {}", d, p);
+                spawn.put(d, p);
+            }
+            catch(NumberFormatException e)
+            {
+                logger.warn("can not completely parse the spawn file : {}", props.getProperty(key));
+            }
+        }
+    }
+
+    public BlockPos getSpawn(World world)
+    {
+        return spawn.getOrDefault(world.provider.getDimension(), world.getSpawnPoint());
+    }
+}
+
+class TeleportToSpawn implements ITeleporter
+{
+
+    @Override
+    public void placeEntity(World world, Entity entity, float yaw)
+    {
+        BlockPos p = Ericius.instance.getSpawn(world);
+        entity.setLocationAndAngles(p.getX(), p.getY(), p.getZ(), entity.rotationYaw, 0.0F);
+        entity.motionX = 0.0D;
+        entity.motionY = 0.0D;
+        entity.motionZ = 0.0D;
+        entity.sendMessage(new TextComponentString("Redirected to spawn..."));
+    }
+
+    @Override
+    public boolean isVanilla()
+    {
+        return false;
+    }
+}
